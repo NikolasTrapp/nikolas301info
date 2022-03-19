233e0ee270ecb22b803e9fe0648d9314a28a428b
NikolasTrapp <nikolastrapp@gmail.com>
2022-03-19 18:18:17 -0300

    Testes

--- projeto/configs.py
+++
@@ -1,8 +0,0 @@
-largura_tela = 1200
-altura_tela = 800
-resolucao = (largura_tela, altura_tela)
-x = 500
-y = 500
-width = 20
-height = 20
-vel = 2
---
+++ projeto/teste.py
@@ -0,0 +1,5 @@
+"""Este é um programa muito complexo capaz de criar
+diversificações na realidade e na forma como a conhecemos
+"""
+
+print('Hello world')
