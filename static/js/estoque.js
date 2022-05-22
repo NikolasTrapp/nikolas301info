7be713081617354044c805c9ad09ccf4d36db87d
trabalhoRPG2022 <101950920+trabalhoRPG2022@users.noreply.github.com>
2022-05-21 22:59:34 -0300

    classe em cada linha da tabela

--- static/js/estoque.js
+++ static/js/estoque.js
@@ -5,19 +5,23 @@ $.ajax(
         success: listar,
         
         error: function(erro){
-            console.log(erro)
+            console.log(erro);
         }
     }
 );
 function listar(equip){
-    $('#tb-equipamentos').html('')
+    var cont = 1;
+    $('#tb-equipamentos').html('');
     for (var i in equip){
         lin = '<tr class="linhas_tabela">' +
-        '<td>' + equip[i].nome + '</td>' +
-        '<td>' + equip[i].local + '</td>' +
-        '<td>' + equip[i].quantidade + '</td>' +
-        '<td>' + equip[i].observacao + '</td>' +
-        '</tr>'
+        '<td class="'+cont+'">' + equip[i].nome + '</td>' +
+        '<td class="'+cont+'">' + equip[i].local + '</td>' +
+        '<td class="'+cont+'">' + equip[i].quantidade + '</td>' +
+        '<td class="'+cont+'">' + equip[i].observacao + '</td>' +
+        '</tr>';
+        btn_editar = '<button type="submit" id="bt-editar'+cont+'">Editar</button>';
+        btn_remover = '<button type="submit" id="bt-remover'+cont+'">Remover</button>';
         $('#tb-equipamentos').append(lin);
-    }            
-};
+        cont += 1;
+    }
+}
